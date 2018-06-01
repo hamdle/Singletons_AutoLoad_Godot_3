b6a48aa02517901b046fb5fc5f8fa34b2d9bd7d5
hamdle <contact@ericmarty.co>
2018-05-31 18:01:42 -0700

    Use code to change start scene at runtime

--- global.gd
+++ global.gd
@@ -6,6 +6,7 @@ var current_scene = null
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
+	goto_scene("res://scene_b.tscn")
 	
 func goto_scene(path):
 	# Use a deferred call to make sure we
